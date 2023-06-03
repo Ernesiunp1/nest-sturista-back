a741c7b8733d53068de1d70d75b0fac304d93221
Ernesto Vivas <vivas.ernesto@gmail.com>
2023-06-03 16:56:17 -0500

    process.env modificado username directo

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import process from 'process';
       host: process.env.DB_HOST,
       port:   +process.env.DB_PORT,
       database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
+      username: 'tvelmjtnsagswr',
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
