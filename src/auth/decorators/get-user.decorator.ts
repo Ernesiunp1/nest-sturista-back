a22bdcab05a1e9ba49d18b92383545a106304f07
Ernesiunp1 <105569926+Ernesiunp1@users.noreply.github.com>
2023-10-06 08:37:10 -0500

    Update get-user.decorator.ts

--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -2,16 +2,7 @@ import { ExecutionContext, createParamDecorator, InternalServerErrorException } 
 
 
 export const GetUser =  createParamDecorator(
-     ( data: string, ctx: ExecutionContext ) => {
-
-        const req = ctx.switchToHttp().getRequest()
-        const user = req.user
-
-        if (!user) {
-            throw new InternalServerErrorException("falta el usuario en la request, no fue encontrado en la req.");
-        }
-            
-        return (!data) ? user : user[data]
+    
      
 
         
